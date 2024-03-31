2addd47d7a612a3d814cfec4b6efa57ee294fcf8
= febilid <= febiolalidyasianturi@gmail.com>
2024-03-31 18:40:24 +0700

    eror berkepanjangan

--- resources/views/m_user/show.blade.php
+++ resources/views/m_user/show.blade.php
@@ -1,61 +1,58 @@
 @extends('m_user/template')
-
-{{-- Custumize layouts section --}}
-
-@section('subtitle', 'User')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'User')
-
 @section('content')
-<div class="container">
-
     <div class="row mt-5 mb-5">
         <div class="col-lg-12 margin-tb">
             <div class="float-left">
-                <h2>CRUD user</h2>
+                <h2> Show User</h2>
             </div>
             <div class="float-right">
-                <a class="btn btn-success" href="{{ route('m_user.create') }}">Input User</a>
+                <a class="btn btn-secondary" href="{{ route('m_user.index') }}">
+                    Kembali</a>
             </div>
         </div>
     </div>
-</div>
-
-@if ($message = Session::get('success'))
-<div class="alert alert-success">
-    <p>{{ $message }}</p>
-</div>
-@endif
-
-<table class="table table-bordered table-responsive d-flex justify-content-center">
-    <tr>
-        <th class="text-center">User id</th>
-        <th class="text-center">level name</th>
-        <th class="text-center">Level id</th>
-        <th class="text-center">Username</th>
-        <th class="text-center">Nama</th>
-        <th class="text-center">Password</th>
-        <th class="text-center">Actions</th>
-    </tr>
-    @foreach ($useri as $m_user)
-    <tr>
-        <td>{{ $m_user->user_id }}</td>
-        <td>{{ $m_user->level->level_nama }}</td>
-        <td>{{ $m_user->level_id }}</td>
-        <td>{{ $m_user->username }}</td>
-        <td>{{ $m_user->nama }}</td>
-        <td>{{ $m_user->password }}</td>
-
-        <td class="text-center">
-            <form action="{{ route('m_user.destroy', $m_user->user_id) }}" method="POST">
-                <a class="btn btn-info btn-sm" href="{{ route('m_user.show', $m_user->user_id) }}">Show</a>
-                <a class="btn btn-primary btn-sm" href="{{ route('m_user.edit', $m_user->user_id) }}">Edit</a>
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>User_id:</strong>
+                {{ $useri->user_id }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Level_id:</strong>
+                {{ $useri->level_id }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Level Kode:</strong>
+                {{ $useri->level->level_kode }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Level Nama:</strong>
+                {{ $useri->level->level_nama }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Username:</strong>
+                {{ $useri->username }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                {{ $useri->nama }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Password:</strong>
+                {{ $useri->password }}
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\MUserController;
-use App\Http\Controllers\POSController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\POSController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,35 +11,43 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome2');
+    return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_Simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
+Route::prefix('/user')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/tambah', [UserController::class, 'tambah']);
+    Route::post('/tambah_simpan', [UserController::class, 'tambah_simpan']);
+    Route::get('/ubah/{id}', [UserController::class, 'ubah']);
+    Route::put('/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+    Route::get('/hapus/{id}', [UserController::class, 'hapus']);
+});
 
+Route::prefix('/kategori')->group(function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+    Route::put('/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+    Route::get('/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.delete');
+});
 
-Route::get('/kategori', [KategoriController::class, 'index'])->name('manage.category');
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('category.create');
-Route::post('/kategori', [KategoriController::class, 'store']);
-Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
-Route::put('/kategori/{id}', [KategoriController::class, 'update']);
-Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('kategori.delete');
+Route::prefix('/level')->group(function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/edit/{id}', [LevelController::class, 'edit'])->name('level.edit');
+    Route::put('/update/{id}', [LevelController::class, 'update'])->name('level.update');
+    Route::get('/delete/{id}', [LevelController::class, 'destroy'])->name('level.delete');
+});
 
 Route::resource('m_user', POSController::class);
-
-Route::get('/formUser', [UserController::class, 'formUser']);
-Route::get('/formLevel', [UserController::class, 'formLevel']);
